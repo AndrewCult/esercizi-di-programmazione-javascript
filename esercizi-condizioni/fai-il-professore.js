0a0374389f531891079c3f8e12361c9f623bd8ac
AndrewCult <cultraro.andrea@gmail.com>
2020-04-20 18:55:37 +0200

    Update fai-il-professore.js

--- esercizi-condizioni/fai-il-professore.js
+++ esercizi-condizioni/fai-il-professore.js
@@ -18,3 +18,51 @@
 
   http://www.imparareaprogrammare.it
 */
+
+var v = 28;
+
+if (v < 18) {
+    v = 1;
+
+} else if (v < 21) {
+    v = 2;
+
+} else if (v < 24) {
+    v = 3;
+
+} else if (v < 27) {
+    v = 4;
+
+} else if (v <= 29) {
+    v = 5;
+
+} else if (v > 30) {
+    console.log('Superato il voto massimo!');
+} else {
+    v = 6;
+
+}
+
+
+switch (v) {
+    case 1:
+        console.log('Insufficiente');
+        break;
+    case 2:
+        console.log('Sufficiente');
+        break;
+    case 3:
+        console.log('Buono');
+        break;
+    case 4:
+        console.log('Distinto');
+        break;
+    case 5:
+        console.log('Ottimo');
+        break;
+    case 6:
+        console.log('Eccellente!');
+        break;
+    default:
+        console.log('Attenzione! Voto non valido!');
+}
